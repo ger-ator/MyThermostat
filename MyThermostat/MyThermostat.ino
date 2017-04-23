508cafd91b74f841e524a956c1deacf4a1ae0442
ger-ator <ger-ator@users.noreply.github.com>
2017-04-23 10:56:08 +0200

    Add files via upload

--- MyThermostat/MyThermostat.ino
+++ MyThermostat/MyThermostat.ino
@@ -31,6 +31,7 @@
  */
 #define EEPROM_V_SETPOINT 0
 #define EEPROM_V_STATUS 4
+#define EEPROM_SLAVE_NODE 5
 
 /*
  * Controlador PID
@@ -52,6 +53,7 @@ DeviceAddress sondaTermometro = {0x28, 0xFF, 0x00, 0xDC, 0x86, 0x16, 0x05, 0x91}
  * Display I2C de 16x2 caracteres
  */
 LiquidCrystal_I2C lcd(0x27, 16, 2);
+bool config_mode;
 
 /*
  * Debouncers para los pulsadores
@@ -64,28 +66,25 @@ Bounce dec = Bounce();
  * S_HEATER
  */
 bool ts_status = false; //true: ON - false: OFF
+bool relay_ack = false; //true: ACK - false: NACK
 int demand;
+uint8_t slave_node;
 MyMessage msg_setpoint(0, V_HVAC_SETPOINT_HEAT);
 MyMessage msg_temp(0, V_TEMP);
 MyMessage msg_status(0, V_STATUS);
-//Los siguientes mensajes son para enviar datos
-//al nodo esclavo que maneja el rele,
-//corresponden a un S_DIMMER pero no lo meto en
-//presentation
-MyMessage msg_demand(1, V_PERCENTAGE);
-MyMessage msg_status_relay(1, V_STATUS);
+MyMessage msg_demand(0, V_PERCENTAGE);
 
 /*
  * Temporizado
  */
 #define DALLAS_SAMPLE_RATE 5100 //ms
 #define PID_SAMPLE_RATE 10000 //ms
+#define LONG_PRESS 2000 //ms
+#define BACKLIGHT_TIME 10000 //ms
 unsigned long timing_dallas;
 unsigned long timing_pid;
-#define SHORT_REFRESH 10000 //ms
-#define LONG_REFRESH 600000 //ms
-unsigned long REFRESH_RATE;
-unsigned long timing_refresh;
+unsigned long timing_pressed;
+unsigned long timing_lcdbacklight;
 
 /*
  * Misc
@@ -95,9 +94,12 @@ unsigned long timing_refresh;
 
 void setup()
 {
-  sp = loadFloat(EEPROM_V_SETPOINT);
+  sp = loadFloat(EEPROM_V_SETPOINT);  
   ts_status = loadState(EEPROM_V_STATUS);
-   
+  slave_node = loadState(EEPROM_SLAVE_NODE);
+  if(isnan(sp) || (sp < 5.0) || (sp > 30.0)) sp = 20.0;
+  if(isnan(slave_node) || slave_node < 0 || slave_node > 255) slave_node = 0;
+     
   /*  
    * Configurar controlador PID.
    * Salida: de 0-100 para actuar como variable para dimmer.
@@ -113,23 +115,17 @@ void setup()
    */
   sensors.begin();
   sensors.setResolution(sondaTermometro, 12);
+  sensors.requestTemperaturesByAddress(sondaTermometro);
+  pv = sensors.getTempC(sondaTermometro);
   sensors.setWaitForConversion(false);
   sensors.requestTemperaturesByAddress(sondaTermometro);
-  sleep(1000);
-  //pv = sensors.getTempC(sondaTermometro);
-  //sensors.requestTemperaturesByAddress(sondaTermometro);
-  pv = 21.0;
+  
 
   /*
    * Iniciar pantalla
    */
   lcd.init();
-  lcd.setCursor(0,0);
-  lcd.print("PV   SP   OUT   ");
-  lcd.setCursor(0,1);
-  lcd.print("                ");
-  if (ts_status) lcd.backlight();
-    else lcd.noBacklight();
+  set_PidMode();
   
   /*
    * Configuracion de pines
@@ -148,21 +144,18 @@ void setup()
    * Enviar estado inicial al controlador y al nodo esclavo
    */
   send(msg_temp.set(pv, 1));
-  send(msg_status.set(ts_status), REQ_ACK);
-  send(msg_setpoint.set(sp, 1), REQ_ACK);
-  send(msg_status_relay.set(ts_status).setDestination(1), REQ_ACK);
+  send(msg_setpoint.set(sp, 1));
+  send(msg_status.set(ts_status).setDestination(0));
+  send(msg_status.set(ts_status).setDestination(slave_node));
 
   timing_dallas = millis();
   timing_pid = millis();
-  timing_refresh = millis();
-  REFRESH_RATE = LONG_REFRESH;
-  
 }
 
 void presentation()
 {
 	sendSketchInfo(SN, SV);
-  present(0, S_HEATER, "Radiador del Salon");
+  present(0, S_HEATER, "Termostato del Salon", REQ_ACK);
 }
 
 
@@ -171,11 +164,10 @@ void loop()
   /*
    * Lectura del sensor de temperatura
    */
-  if((unsigned long)(millis() - timing_dallas) > DALLAS_SAMPLE_RATE) {
+  if((unsigned long)(millis() - timing_dallas) >= DALLAS_SAMPLE_RATE) {
     double last_pv = pv;
-    //pv = sensors.getTempC(sondaTermometro);
-    //sensors.requestTemperaturesByAddress(sondaTermometro);
-    pv = 22.0;
+    pv = sensors.getTempC(sondaTermometro);
+    sensors.requestTemperaturesByAddress(sondaTermometro);
     if (pv != last_pv) {
       send(msg_temp.set(pv, 1));
     }
@@ -185,43 +177,47 @@ void loop()
   /*
    * Evaluacion del PID
    */
-  if ((unsigned long)(millis() - timing_pid) > PID_SAMPLE_RATE) {
+  if ((unsigned long)(millis() - timing_pid) >= PID_SAMPLE_RATE) {
     if(!isnan(pv) && !isnan(sp)) {
       int lastDemand = demand;
       if (myPID.Compute()) {
         demand = (int) out;
-        if (demand != lastDemand) send(msg_demand.set(demand).setDestination(1));
+        if (relay_ack) {
+          if (demand != lastDemand) send(msg_demand.set(demand).setDestination(slave_node));
+        } else {
+          relay_ack = send(msg_status.set(ts_status).setDestination(slave_node), REQ_ACK);
+          if (relay_ack) send(msg_demand.set(demand).setDestination(slave_node));
+        }
       }        
     }    
     timing_pid = millis();
   }
 
   /*
-   * Refresco de datos en controlador y esclavo
+   * Temporizado de la retroiluminacion
    */
-  if((unsigned long)(millis() - timing_refresh) > REFRESH_RATE) {
-    Serial.println("Refresco");
-    bool ackok;
-    ackok = send(msg_status.set(ts_status), REQ_ACK);
-    ackok = ((send(msg_status_relay.set(ts_status).setDestination(1), REQ_ACK)) && ackok);
-    ackok = ((send(msg_setpoint.set(sp, 1), REQ_ACK)) && ackok);
-    if(ackok) REFRESH_RATE = LONG_REFRESH;
-    timing_refresh = millis();
-  }   
+  if ((unsigned long)(millis() - timing_lcdbacklight) >= BACKLIGHT_TIME) {
+    lcd.noBacklight();
+  }
 
   /*
    * Actualiza display
    */
-  lcd.setCursor(15,0);
-  if (REFRESH_RATE == LONG_REFRESH) lcd.print("S");
-  else lcd.print("F");
-  lcd.setCursor(0,1);
-  lcd.print(pv, 1);
-  lcd.setCursor(5,1);
-  lcd.print(sp, 1);
-  lcd.setCursor(10,1);
-  lcd.print(demand);
-  lcd.print("  ");
+  if (config_mode) {
+    lcd.setCursor(0,1);
+    lcd.print(slave_node);
+    lcd.print("  ");        
+  } else {
+    lcd.setCursor(6, 0);
+    lcd.print(pv, 1);
+    lcd.setCursor(4, 1);
+    lcd.print(sp, 1);
+    lcd.setCursor(13, 1);
+    lcd.print(demand);
+    lcd.print("  ");
+  }
+  lcd.setCursor(13,0);
+  lcd.print((ts_status ? "ON " : "OFF"));
 
   /*
    * Pulsadores
@@ -229,15 +225,51 @@ void loop()
   onoff.update();
   inc.update();
   dec.update();
-  
-  if(inc.fell()) {
-    setV_Setpoint(sp + SP_INCDEC);
+
+  if(inc.fell() || dec.fell() || onoff.fell()) {
+    lcd.backlight();
+    timing_lcdbacklight = millis();
   }
-  if(dec.fell()) {
-    setV_Setpoint(sp - SP_INCDEC);
+
+  if(inc.fell() && dec.fell()) {
+    set_ConfigMode();
   }
-  if(onoff.fell()) {
-    setV_Status(!ts_status);
+
+  if (config_mode) {
+    if(inc.fell()) {
+      slave_node++;
+    }
+    if(dec.fell()) {
+      slave_node--;
+    }
+    if(onoff.fell()) {
+      saveState(EEPROM_SLAVE_NODE, slave_node);
+      set_PidMode();
+    }    
+  } else {
+    if(inc.fell()) {
+      float new_sp = sp + SP_INCDEC;
+      if (new_sp <= 30.0) {
+        send(msg_setpoint.set(new_sp, 1), REQ_ACK);        
+      }
+    }
+    if(dec.fell()) {
+      float new_sp = sp - SP_INCDEC;
+      if (new_sp >= 5.0) {
+        send(msg_setpoint.set(new_sp, 1), REQ_ACK);        
+      }
+    }
+    if(onoff.fell()) {
+      timing_pressed = millis();
+    }
+    if(onoff.rose()) {
+      if((unsigned long)(millis() - timing_pressed) >= LONG_PRESS) {
+        //EJECUTAR ESCENA APAGAR TODOS LOS NODOS
+        Serial.println("pulsacion larga");
+      } else {
+        send(msg_status.set(!ts_status).setDestination(0), REQ_ACK);
+      }
+    }
   }
 }
 
@@ -246,12 +278,15 @@ void loop()
  * en posiciones de EEPROM
  */
 void saveFloat(const uint8_t pos, const float value) {
+  Serial.println("Llamada a savefloat");
   union float_bytes {
     float val;
     byte bytes[sizeof(float)];
   } data;
   data.val = value;
   for (int i = 0; i < sizeof(float); i++) {
+    Serial.print("Almaceno byte ");
+    Serial.println(i + 1);
     saveState(pos + i, data.bytes[i]);
   }  
 }
@@ -271,37 +306,47 @@ float loadFloat (const uint8_t pos) {
 }
 
 /*
- * Cambia de estado encendido/apagado
+ * set_ConfigMode
  */
-void setV_Status (bool new_ts_status) {
-  if (ts_status != new_ts_status) {
-    ts_status = new_ts_status;
-    saveState(EEPROM_V_STATUS, ts_status);
-    myPID.SetMode(ts_status ? AUTOMATIC : MANUAL);
-    if(send(msg_status.set(ts_status), REQ_ACK) == false) REFRESH_RATE = SHORT_REFRESH;
-    if(send(msg_status_relay.set(ts_status).setDestination(1), REQ_ACK) == false) REFRESH_RATE = SHORT_REFRESH;;
-    if(ts_status) lcd.backlight();
-      else lcd.noBacklight();
-  }
+void set_ConfigMode (void) {
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Slave Node: ");
+  config_mode = true;  
 }
 
 /*
- * Fijar el valor de setpoint
+ * set_ConfigMode
  */
-void setV_Setpoint(float setpoint) {
-  if (sp != setpoint) {
-    sp = setpoint;
-    saveFloat(EEPROM_V_SETPOINT, sp);
-    if(send(msg_setpoint.set(sp, 1), REQ_ACK) == false) REFRESH_RATE = SHORT_REFRESH;    
-  }
+void set_PidMode (void) {
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Temp:           ");
+  lcd.setCursor(0,1);
+  lcd.print("Sp:      Dm:    ");
+  config_mode = false;  
 }
 
 void receive(const MyMessage &message)
 {
   if (message.type == V_HVAC_SETPOINT_HEAT) {
-    setV_Setpoint(message.getFloat());
-  } else if (message.type == V_STATUS) {
-    setV_Status(message.getBool());
-    
+    float new_sp = message.getFloat();
+    if ((sp != new_sp) && (new_sp >= 5.0) && (new_sp <= 30.0)) {
+      sp = new_sp;
+      saveFloat(EEPROM_V_SETPOINT, sp);
+      //Si el mensaje NO es ACK quiere decir que se ha enviado el setpoint 
+      //desde el controlador. Domoticz no actualiza el valor cuando se envia
+      //aun estando configurado ACK, luego hay que enviarle el 
+      //nuevo valor al controlador
+      if (!message.isAck()) send(msg_setpoint.set(sp, 1));
+    }    
+  } else if ((message.type == V_STATUS) && (message.sender == 0)) {
+    bool new_ts_status = message.getBool();
+    if (ts_status != new_ts_status) {
+      ts_status = new_ts_status;
+      saveState(EEPROM_V_STATUS, ts_status);
+      myPID.SetMode(ts_status ? AUTOMATIC : MANUAL);
+      relay_ack = send(msg_status.set(ts_status).setDestination(slave_node), REQ_ACK);
+    }
   }
 }
